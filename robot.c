a7edc00eda116c918d93091b0d44282d1989cd28
LaFave <lafave22@egr-linux-6330.campus.up.edu>
2019-12-03 10:31:24 -0800

    game the robot a makeover. Add to author.

--- robot.c
+++ robot.c
@@ -4,7 +4,7 @@
  * A file that contains the supporting functions for main() in the
  * ~/robots example source for Software Engineering.
  *
- * @author Tanya L. Crenshaw, Nathan Schmedake
+ * @author Tanya L. Crenshaw, Nathan Schmedake, noah lafave
  * @since August 2013
  *
  */
@@ -35,10 +35,10 @@ void robotPrintAscii(void)
   blank = blank +30;
   printf("\n\n"
 	 "UWR: Unidentified Wheeled Robot\n"
-	 "%s    i_i    \n"
-	 "%s   [@_@]   \n"
+	 "%s    (_)    \n"
+	 "%s   [I_I]   \n"
 	 "%s  /|___|\\ \n"
-	 "%s   d   b   \n",blank, blank, blank, blank);
+	 "%s    d b    \n",blank, blank, blank, blank);
 
   blank = blank - 10;
 }
@@ -62,7 +62,7 @@ void dalekPrintAscii(void)
          "%s    o  0  ()  o    \n"
 	 "%s   o           o   \n"
    	 "%s o o o o o o o o o \n"
-   	 "%s o o o o o o o o o \n"
+   	 "%s m y n a m e jeffo \n"
    	 "%s o o o o o o o o o \n", blank, blank, blank, blank, blank, blank, 
 	 blank, blank, blank, blank);
   blank = blank - 10;
